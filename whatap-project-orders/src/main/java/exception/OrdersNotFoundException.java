3cb5ab4bca5698436cdc7dbd2c8f0bcb8e1fc888
김관진 <cozyschooll@gmail.com>
2024-03-11 14:15:19 +0900

    Chore: subtree 삭제

--- whatap-project-orders/src/main/java/exception/OrdersNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class OrdersNotFoundException extends CustomException {
-    public OrdersNotFoundException() {
-        super("해당 주문을 찾을 수 없습니다.");
-    }
-}
--- whatap-project-orders/src/main/java/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class ProductNotFoundException extends CustomException {
-    public ProductNotFoundException() {
-        super("해당 상품을 찾을 수 없습니다.");
-    }
-}
